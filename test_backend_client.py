7eaa06d93ba147dd7a4c0c6ea60b69ed5c00014e
Larry Darrelc <larrydarrelc@gmail.com>
2014-02-19 23:03:36 +0800

    Add warnning message generate client.

--- test_backend_client.py
+++ test_backend_client.py
@@ -1,19 +1,33 @@
 #coding: utf-8
 
 import time
+import json
+import random
+from hashlib import sha1
 
 from backend.utils import send_to_backend
+
+
+def warnning_message():
+    '''Generate a random warning message.'''
+    levels = ['DEBUG', 'INFO', 'WARN', 'FATAL']
+    message = {
+        'level': random.choice(levels),
+        'message': sha1(str(time.time())).hexdigest(),
+        'time': time.time()
+    }
+    return json.dumps(message)
 
 
 def run():
     client = None
     try:
         while True:
-            client = send_to_backend('hellox', 'world', client)
-            time.sleep(2)
-    except Exception as e:
+            client = send_to_backend('warn', warnning_message(), client)
+            time.sleep(5)
+    except Exception:
         client.close()
-        raise e
+        raise
 
 
 if __name__ == '__main__':
