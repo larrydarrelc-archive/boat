d9df1e8d44c466ac8857c8c550376220e76118f7
Larry Darrelc <larrydarrelc@gmail.com>
2014-02-26 13:37:20 +0800

    Update schema to support item service.
    TODO Create a script for running migrations.

---
+++ data/2.items.sql
@@ -0,0 +1,23 @@
+DROP TABLE IF EXISTS `items`;
+CREATE TABLE `items` (
+    `id` INTEGER NOT NULL,
+    /* 0 is normal, 1 is warning */
+    `status` INTEGER NOT NULL default 0,
+    `data` VARCHAR(512) NOT NULL default "",
+    `meta` VARCHAR(1024),
+
+    `created_at` DATETIME default current_timestamp,
+    `updated_at` DATETIME default current_timestamp
+);
+
+DROP TABLE IF EXISTS `logging`;
+CREATE TABLE `logging` (
+    `id` INTEGER PRIMARY KEY,
+    `item_id` INTEGER NOT NULL,
+    /* 0 is triggered, 1 is confirmed, 2 is disappeared */
+    `status` INTEGER NOT NULL default 0,
+
+    `triggered_at` DATETIME default current_timestamp,
+    `confirmed_at` DATETIME default current_timestamp,
+    `disappeared_at` DATETIME default current_timestamp
+);
